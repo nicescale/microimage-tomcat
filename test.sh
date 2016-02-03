da3e27f206289e316257f02ff4f44efb07c75c5c
shellrock <hanwoody@gmail.com>
2016-02-03 19:05:07 +0800

    fix test

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ fi
 docker cp /tmp/sample.war $CON_NAME:/app/
 sleep 2
 docker exec $CON_NAME ps ax|grep "tomca[t]"
-docker exec $CON_NAME wget -O - http://localhost/sample/|grep "Hello, World"
+docker exec $CON_NAME wget -O - http://localhost:8080/sample/|grep "Hello, World"
 
 docker rm -f $CON_NAME
 
