9531415079224b9383aaeb502622dd3683118f3d
shellrock <hanwoody@gmail.com>
2016-02-03 14:20:19 +0800

    add test for tomcat

--- build.sh
+++ build.sh
@@ -3,21 +3,21 @@
 set -e
 
 export CON_NAME=tomcat_t
-export REG_URL=d.nicescale.com:5000
-export IMAGE=tomcat
+export REG_URL=index.csphere.cn
+export IMAGE=microimages/tomcat
 export TAGS="8.0 8.0.30"
 export BASE_IMAGE=microimages/jre
 
 docker pull $BASE_IMAGE
 
-docker build -t microimages/$IMAGE .
+docker build -t $IMAGE .
+./test.sh
 
-#./test.sh
-
-echo "---> Starting push microimages/$IMAGE:$VERSION"
-
+docker tag -f $IMAGE $REG_URL/$IMAGE
 for t in $TAGS; do
-  docker tag -f microimages/$IMAGE microimages/$IMAGE:$t
+  docker tag -f $IMAGE $REG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IMAGE:$t
 done
 
-docker push microimages/$IMAGE
+docker push $IMAGE
+docker push $REG_URL/$IMAGE
---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+docker rm -f "$CON_NAME" > /dev/null 2>&1
+docker run -d --name $CON_NAME $IMAGE
+
+if [ ! -f /tmp/sample.war ]; then
+  wget -O /tmp/sample.war https://tomcat.apache.org/tomcat-8.0-doc/appdev/sample/sample.war
+fi
+docker cp /tmp/sample.war $CON_NAME:/app/
+sleep 2
+docker exec $CON_NAME ps ax|grep "tomca[t]"
+docker exec $CON_NAME wget -O - http://localhost/sample/|grep "Hello, World"
+
+docker rm -f $CON_NAME
+
+echo "---> The test pass"
